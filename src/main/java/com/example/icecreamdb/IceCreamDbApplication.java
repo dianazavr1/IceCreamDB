ac2f0b790050a7ece68ec16ca5b2afa02571eaef
dianazavr1 <2235.10002@manas.edu.kg>
2025-03-04 12:09:36 +0600

    first commit

---
+++ src/main/java/com/example/icecreamdb/IceCreamDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.icecreamdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceCreamDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IceCreamDbApplication.class, args);
+    }
+
+}
